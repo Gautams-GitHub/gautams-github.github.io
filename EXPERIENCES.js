1b9eeab7dc4ee6d01277b06966db3c9629d13fc6
Gautams-GitHub <gagan.off.the.grid@gmail.com>
2025-09-12 18:40:18 +0530

    Add experince urls

--- EXPERIENCES.js
+++ EXPERIENCES.js
@@ -1,1 +1,46 @@
- 
+const experiences=[
+    {
+        link: "https://www.lemarcheretail.com/",
+        title: "https://www.lemarcheretail.com/",
+        description: "What you did",
+        tags: ["Tag1", "Tag2"]
+      },
+    {
+      link: "https://satyaretail.com/home",
+      title: "https://satyaretail.com/home",
+      description: "What you did",
+      tags: ["Tag1", "Tag2"]
+    },
+    {
+        link: "https://naturesmiraclestore.com/",
+        title: "https://naturesmiraclestore.com/",
+        description: "What you did",
+        tags: ["Tag1", "Tag2"]
+      },
+      {
+        link: "https://statica.payuinnovations.in/login",
+        title: "https://statica.payuinnovations.in/login",
+        description: "What you did",
+        tags: ["Tag1", "Tag2"]
+      },
+      {
+        link: "https://play.google.com/store/apps/details?id=app.marcheretail",
+        title: "Play Store - Le Marche ",
+        description: "What you did",
+        tags: ["Tag1", "Tag2"]
+      },
+      {
+        link: "https://apps.apple.com/in/app/lemarche/id1552171030",
+        title: "App Store - Le Marche ",
+        description: "What you did",
+        tags: ["Tag1", "Tag2"]
+      },{
+        link: "https://apps.apple.com/in/app/natures-miracle/id6443844555",
+        title: "App Store - Nature's Miracle ",
+        description: "What you did",
+        tags: ["Tag1", "Tag2"]
+      },
+
+  ];
+
+  export default experiences;
